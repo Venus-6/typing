0d7da6c996b59739e362cdd27534fe42dae49a2c
Erlion <71623672+Venus-6@users.noreply.github.com>
2022-06-16 06:03:32 +0400

    Update LevelRepository.java

--- src/main/java/com/example/typing/repos/LevelRepository.java
+++ src/main/java/com/example/typing/repos/LevelRepository.java
@@ -1,2 +1,8 @@
-package com.example.typing.repos;public interface LevelRepository {
+package com.example.typing.repos;
+
+import com.example.typing.domain.Level;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LevelRepository extends CrudRepository<Level, Long> {
+    Level findLevelById(long id);
 }
